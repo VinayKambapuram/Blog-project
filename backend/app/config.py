06b6e14de1ee19e689fe0a4b7ed4f6e783f268b6
Git-User-Vinay <vinaykumarkambapuram@gmail.com>
2025-01-22 09:45:52 +0530

    updated db config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 #Mongo URI
-MONGO_URI = "mongodb://vinaykumar:j0oom6fWJ69xd452@cluster0.j7zn5.mongodb.net/"
+MONGO_URI = "mongodb://<username>:<password>@cluster0.j7zn5.mongodb.net/"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client["test"] # Database name
